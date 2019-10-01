3a3c5019c4ff01b47b58fd69b49dedf753a51a7e
Andy Tisdall <andrew.tisdall@gmail.com>
2019-10-01 02:04:13 -0700

    fuck

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -129,9 +129,10 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
+MEDIA_URL = '/media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-MEDIA_URL = '/media/'
 
 try:
     from .local_settings import *
