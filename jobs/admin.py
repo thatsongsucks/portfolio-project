921a96650a2885eaf9a6521749f1abd60f166c80
thatsongsucks <andrew.tisdall@gmail.com>
2020-10-06 20:19:19 -0700

    registered demo

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Job, Show, MailingListPerson
+from .models import Job, Show, MailingListPerson, Demo
 
 admin.site.register(Job)
 admin.site.register(Show)
 admin.site.register(MailingListPerson)
+admin.site.register(Demo)
